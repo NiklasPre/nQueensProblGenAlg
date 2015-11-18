e722623ef37eb309842e5b66e393e268fe68f2ca
Niklas Preschern <m.preschern@gmx.at>
2015-11-19 00:34:21 +0100

    Added Comments
    Comments, Fitness Checks

--- nQueensProblem/Population.h
+++ nQueensProblem/Population.h
@@ -1,38 +1,129 @@
+//Not tested
+
 #include <iostream>
 #include <stdlib.h>
 
-template <int N>
+/*
+ * Class defines Population for the algorithm;
+ * template parameter N sets n in n-Queens Problem
+ */
+template <size_t N>
 class Population {
 
-  template <int N>
+  /*
+   * Indiviual of the Population
+   * m_iFitnness - Integer that represents the viability to use in Mutation/Crossover
+   * m_rgQueenPositions - Integer Array with positions of Queens
+   */
   struct ChessBoard {
     int m_iFitness;
     int m_rgQueenPositions[N-1];
 
-    ChessBoard(): m_iFitness(0) {
-      for(size_t i = 0; i < N; i++) {
-        m_rgQueenPositions[i] = std::rand() % N;
-      }
-    };
-
-    void fnCheckFitness() {
-      for(size_t i = 0; i < N; i++) {
-        for(size_t j = 0; j < N-i; j++) {
-          if(m_rgQueenPositions[i] == m_rgQueenPositions[j]) {
-            m_iFitness--;
-          }
-        }
+    /*
+     * Constructor - Randomizes position of queens
+     */
+    ChessBoard() {
+      m_iFitness = 0;
+      for(int i = 0; i < N; i++) {
+        m_rgQueenPositions[i] = rand() % N;
       }
     }
+
+    /*
+     * Function to set the fitness var
+     */
+    void fnCheckFitness() {
+      m_iFitness -= fnCheckRow() + fnCheckDiag();
+    }
+
+    int fnCheckRow();
+    int fnCheckDiag(); // TODO: combine with fnCheckRow; OR use Threads?
+    //TODO: Destructor
+
   };
 
+  /*
+   * Dynamic array that holds the Indiviuals (TODO: use vector instead?)
+   */
   ChessBoard **m_rgcbPopulation;
 
 public:
+  Population();
+  void fnRateFitness();
+  virtual std::ostream& print(std::ostream& o) const;
+
+/*
+ * Constructor - Fills member Array with 50 Individuals (TODO: 50?!)
+ */
+
+
+};
+
+template <size_t N>
 Population<N>::Population() {
-  for(size_t i = 0; i < 50; i++) {
-    m_rgcbPopulation[i] = new ChessBoard;
+  for(int i = 0; i < 50; i++) {
+    m_rgcbPopulation[i] = new ChessBoard();
   }
 }
 
-};
+template <size_t N>
+void Population<N>::fnRateFitness() {
+  for(int i = 0; i < 50; i++) {
+    m_rgcbPopulation[i]->fnCheckFitness();
+  }
+
+}
+
+/*
+ * Checks if a queen is the only one in a vertical/horizontal row
+ * counter to modify fitness var in Individuals
+ */
+template <size_t N>
+int Population<N>::ChessBoard::fnCheckRow() {
+  int i_cFitCountRow;
+
+  for(int i = 0; i < N; i++) {
+    for(int j = i+1; j < N-i; j++) { //TODO: if i == N / Array out of bound
+      if(m_rgQueenPositions[i] == m_rgQueenPositions[j]) {
+        i_cFitCountRow++;
+      }
+    }
+  }
+
+return i_cFitCountRow;
+}
+
+/*
+ * Checks if a queen is the only one in a diagonal row
+ * counter to modify fitness var in Individuals
+ */
+template <size_t N>
+int Population<N>::ChessBoard::fnCheckDiag() {
+  int i_cFitCountDiag;
+
+  for(int i = 0; i < N; i++) {
+    for(int j = i+1; j < N-i; j++) {
+      if(m_rgQueenPositions[j] == m_rgQueenPositions[i]+j || m_rgQueenPositions[j] == m_rgQueenPositions[i]-j) {
+        i_cFitCountDiag++;
+      }
+
+    }
+  }
+
+
+
+  return i_cFitCountDiag;
+}
+
+template<size_t N>
+std::ostream& Population<N>::print(std::ostream& o) const {
+  o << "Population, N= " << N << '\n';
+  for(int i = 0; i < 10; i++) {
+    o << "Individual " << i << ": " << '\n';
+    for(int j = 0; j < 5; j++) {
+      o << "QueensPos: [" << m_rgcbPopulation[i]->m_rgQueenPositions[j] << ' ' << '\n';
+    } o << ']' << '\n';
+  }
+
+  return o;
+}
--- nQueensProblem/nQueensProblem.cpp
+++ nQueensProblem/nQueensProblem.cpp
@@ -1,12 +1,27 @@
 // nQueensProblem.cpp : Defines the entry point for the console application.
 //
+#ifndef POPULATION_H
+#define POPULATION_H
 
 #include <iostream>
 #include "Population.h"
 
-int main()
-{
+int main() {
+
+  Population<5>* population = NULL;
+  const char helpStr[] =
+    "This is a Helpstr\n"
+    "It is here to help.\n";
+
+  population = new Population<5>;
+  population->fnRateFitness();
+
+  population->print(std::cout); // TODO: Segfault, probably ChessBoard ** in Population.h
+
 
 
   return 0;
+
 }
+
+#endif //POPULATION_H
